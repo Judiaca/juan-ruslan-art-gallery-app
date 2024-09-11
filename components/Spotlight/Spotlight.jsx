966cbda6d596ffb57d2b95f5434e4b4fac995221
Ruslan <info@mirasov.dev>
2024-09-11 14:20:41 +0200

    fix some semantic bugs

--- components/Spotlight/Spotlight.jsx
+++ components/Spotlight/Spotlight.jsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 const Spotlight = ({ image, artist }) => {
   return (
     <div>
-      {image && <Image src={image} width={400} height={400} alt={artist} />}
+      {image && (
+        <Image src={image} width={400} height={400} alt={artist} priority />
+      )}
       <p>
         Artist: <span>{artist}</span>
       </p>
